db1a360450838611abe0f32df1870db640a5baff
Serghei Grajdean <grajdeanserghei@gmail.com>
2017-12-03 02:03:20 +0200

    programatic queen added

---
+++ src/n-queens.asd
@@ -0,0 +1,7 @@
+(defsystem "n-queens"
+  :description "nqueens problem"
+  :version "0.0.1"
+  :author "Serghei Grajdean <grajdeanserghei@gmail.com>"
+  :licence "GNU"  
+  :components ((:file "n-queens")
+               (:static-file "images/queen.png")))
--- src/n-queens.lisp
+++ src/n-queens.lisp
@@ -25,12 +25,16 @@
     (let* ((*debug-tk* nil)
 	   (c (make-instance 'canvas :width 400 :height 300))
 	   (board-settings (get-board-settings n))
-	  (table (loop for i from 1 to n
+	   (squares (loop for i from 1 to n
 		      nconc (loop for j from 1 to n
 			       collect(draw-square i j c board-settings))))
-          )
+	   (board-model (make-hash-table)))
      (pack c :expand 1 :fill :both)
      (scrollregion c 0 0 800 800)
+     (setf (gethash 'squares board-model) squares)
+     (setf (gethash 'canvas board-model) c)
+     (draw-queen 1 1 c)
+     board-model
      )))
 
 (defun draw-square(x y canvas board-settings)
@@ -45,12 +49,46 @@
 	 (color (if (oddp (+ x y)) "black" "white"))
 	 )
     (itemconfigure canvas square "fill" color)
+    square
     ))
 
 (defun draw-queen(x y canvas)
-  
+  (let* ((queen-coords )
+	 (queen (create-polygon canvas '(10 70 70 70 60 60 70 45 75 12 60 40 58 6 47 40 40 2 32 40 21 6 17 40 3 12 5 40 18 60 10 70)))
+	 (c (canvas canvas))
+;;	 (image-path (merge-pathnames "images/queen.png" *load-truename*))
+	 (image-path "C:/work/n-queens/src/images/queen.png")
+;;	 (image-path "images/queen.png")
+	 (img (make-image)))    
+;;    (image-load img image-path)
+;;    (create-image c x y :image img)
+;;    (itemconfigure c img "width" 30)
+;;    (configure c :borderwidth 2 :relief :sunken)
+;;    (pack c )
+#||
+    (let* ((queen-img (create-image c x y :image image)))
+	   (itemconfigure c image "width" 30)
+	   )
+||#
+;;	 (configure c :borderwidth 2 :relief :sunken)
+;;	 (scale c 0.25)
+    queen)
   )
+
+(defun get-queen-image()
+  (let* ((image (make-image)))
+  (image-load image "C:/work/n-queens/src/images/queen.png"))
+  )
+
+(get-queen-image)
     
 (draw-chessboard 8)
 
 (get-board-settings 4)
+
+*load-pathname*
+*buffer-file-name*
+
+
+(ltktest)
+
